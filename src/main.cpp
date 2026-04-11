547323f2ee134ee3e2b8b86da0182e6571c643b1
shinkuan <shinkuan318@gmail.com>
2026-04-11 13:11:59 +0800

    Add toggle functionality for waifuland using SIGUSR1 signal

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,14 @@
 
 int main(int argc, char* argv[])
 {
+    // waifuland toggle
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "toggle") == 0 || strcmp(argv[i], "--toggle") == 0) {
+            system("killall -s SIGUSR1 waifuland");
+            return 0;
+        }
+    }
+
     // Parse models_dir
     std::string modelsDir = "";
     for (int i = 1; i < argc; ++i) {
